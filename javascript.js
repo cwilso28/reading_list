a1e62564eb9ea2d19c0f75807e7410da4bd36c7c
Chris <47066922+cwilso28@users.noreply.github.com>
2025-02-14 21:03:07 -0600

    Create arrays for future table writing tests

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let full_test_array = [("The Hobbit", "J.R.R. Tolkien", "310", createCheckbox, createDelete),
+                  ("The Lion, the Witch and the Wardrobe", "C.S. Lewis", "172", createCheckbox, createDelete),
+                  ("Leviathan Wakes", "James S.A. Corey", "577", createCheckbox, createDelete)
+                  ("A Game of Thrones", "George R.R. Martin", "694", createCheckbox, createDelete)
+];
+
+let partial_test_array = [("The Hobbit", "J.R.R. Tolkien", "310", "yes", "delete"),
+    ("The Lion, the Witch and the Wardrobe", "C.S. Lewis", "172", "no", "delete"),
+    ("Leviathan Wakes", "James S.A. Corey", "577", "yes", "don't delete")
+    ("A Game of Thrones", "George R.R. Martin", "694", "no", "don't delete")
+];
